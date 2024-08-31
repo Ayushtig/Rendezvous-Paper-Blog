5a399faabaa01c7aa38a458acf609a638896eeba
Ayush Acharya <86613525+Ayushtig@users.noreply.github.com>
2024-08-31 23:27:55 +0530

    Add files via upload

---
+++ backend/src/db.js
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+
+let db;
+
+async function connectToDb(cb){
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.2i1mx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    await client.connect();
+    db = client.db("react-blog-db");
+    cb()
+}
+
+export{
+    db,
+    connectToDb,
+}
