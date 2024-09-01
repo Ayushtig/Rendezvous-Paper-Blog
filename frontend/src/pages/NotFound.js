d9bdab9609863156b73fae0413a4f15be8ff4586
Ayush Acharya <86613525+Ayushtig@users.noreply.github.com>
2024-09-01 22:43:46 +0530

    Delete frontend directory

--- frontend/src/pages/NotFound.js
+++
@@ -1,8 +0,0 @@
-const NotFound = () => (
-    <div className="notfound">
-        <img src="https://c4.wallpaperflare.com/wallpaper/649/338/357/morpheus-the-matrix-wallpaper-preview.jpg" alt="" />
-        <h1>404</h1>
-    </div>
-);
-
-export default NotFound;
